369d995995d95c8e89444475223d774e1043e857
r0stiars0 <budi@rostiarso.com>
2023-01-13 21:25:11 +0100

    title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { MetaFunction } from "@remix-run/node";
 import HeroSection from "~/components/hero-section";
 import MainTiles from "~/components/main-tiles";
 
@@ -9,3 +10,15 @@ export default function Index() {
     </>
   );
 }
+
+export const meta: MetaFunction = () => {
+
+  return {
+    title: "Aliya Demo Web",
+    "og:title": "Aliya Demo Web",
+    "og:description": "demo web Aliya Seruni Puti",
+    "og:image": "https://security.byrontang.repl.co/htmlbestanden/stylesheets/imagesheets/groenetekst.jpeg",
+    "og:type": "website",
+    "og:url": "https://aliya-demo-web.netlify.app/",
+  };
+};
